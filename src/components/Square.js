b7938e5d7b40eef8ce45c046c5f52959a8d73734
BrodyLinux <Frostwizard7987@gmail.com>
2022-06-14 14:30:54 -0500

    Add components for Tic-tac-toe logic

---
+++ src/components/Square.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Square = props => {
+  return (
+    <div>Square</div>
+  )
+}
+
+Square.propTypes = {}
+
+export default Square
