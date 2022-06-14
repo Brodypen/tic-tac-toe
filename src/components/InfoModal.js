858d53bfa68554f2862c96c3d858c118a93cd966
BrodyLinux <Frostwizard7987@gmail.com>
2022-06-14 14:29:24 -0500

    Add InfoModal

---
+++ src/components/InfoModal.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import {
+  Text,
+  Link,
+  useDisclosure,
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from '@chakra-ui/react';
+
+const InfoModal = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <>
+      <Button onClick={onOpen}>Information</Button>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay bg='blackAlpha.300' />
+        <ModalContent>
+          <ModalHeader>Info about project</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Text fontSize="md">
+              Howdy! This is a project made following the{' '}
+              <Link
+                href="https://www.theodinproject.com/"
+                isExternal
+                color="teal.500"
+              >
+                Odin Project{<br></br>}
+              </Link>
+              This will be my first React and Chakra UI project{<br></br>}Thank you for playing!
+            </Text>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default InfoModal;
